50d6040e3bfef8fc924eda4ab1b0e16914570e46
VQuine <01010110.v@gmail.com>
2021-05-12 00:04:45 -0400

    commit tree a70aabcab4869917450e69541d7698b1295382b5

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render 'birds/index.html.erb'
+    # render 'birds/index.html.erb' 
+    # FOR ---- HTML w/ RUBY CODE EMBEDDED IN ----- EMBEDDED RUBY *V I E W S*
+    # render plain: "Hello #{@birds[3].name}" 
+    # TO RENDER PLAINTEXT
+    # render json: { key: 'arra of data will get converted to JSON' }
+    render json: @birds
+
+    
   end
 end
